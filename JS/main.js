268630d047c3a430701c70d35cafd8f74ee98d50
Carlos Rodríguez <racm1698@gmail.com>
2022-10-10 17:59:41 -0600

    Inicio de la version-Mobile

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,11 @@
 const btnCompartir= document.querySelector('#share');
 const socialMedia=document.querySelector('.card__socialMedia');
-console.log(socialMedia);
+
+
 btnCompartir.addEventListener('click',()=>{
     socialMedia.classList.add("display");
     setTimeout(() => {
         socialMedia.classList.remove("display");
     }, 3000);
 });
+
