dd1c8af94db2fdc9acfbd48603ff29110bbafc40
Carlos Rodríguez <racm1698@gmail.com>
2022-10-20 13:02:26 -0600

    Agregando mas fucinalidades a la seccion de compartir

--- designMobile-Only/Js/main.js
+++ designMobile-Only/Js/main.js
@@ -2,8 +2,13 @@ const bntShare = document.querySelector('#bntShare');
 const shareContainer = document.querySelector(".post__left");
 const shareContainerLeft=document.querySelector(".post__share");
 const iconsContainer = document.querySelector(".post__icons");
-
 bntShare.addEventListener('click',rebuild);
+
+function eliminarIcons(){
+    while(iconsContainer.firstChild){
+        iconsContainer.removeChild(iconsContainer.firstChild);
+    }
+}
 
 function eliminarSeccion(){
     while(shareContainer.firstChild){
@@ -18,9 +23,29 @@ function eliminarContainer(){
 }
 
 function rebuild(){
-    iconsContainer.classList.add("display");
+    
     eliminarSeccion();
     eliminarContainer();
+
+    const btnAuxContainer = document.createElement("div");
+    btnAuxContainer.classList.add("post__btn");
+        
+    const btnShareAux=document.createElement("span");
+    btnAuxContainer.appendChild(btnShareAux);
+
+    iconsContainer.classList.add("display");
+    
+    console.log(iconsContainer.children.length);
+    const elimaritem=document.querySelector(".post__btn");
+
+    if(iconsContainer.children.length === 4){
+        console.log('Lo elimine');
+        iconsContainer.appendChild(btnAuxContainer);
+    }else{
+        iconsContainer.removeChild(elimaritem);
+    }
+    console.log(iconsContainer.children.length);
+
     setTimeout(() => {
         iconsContainer.classList.remove("display");
         const divAux=document.createElement("div");
@@ -38,7 +63,6 @@ function rebuild(){
         btnAuxContainer.classList.add("post__btn");
         
         const btnShareAux=document.createElement("span")
-        btnShareAux.setAttribute("id","bntShare");
         btnShareAux.addEventListener('click',rebuild)
        
         btnAuxContainer.appendChild(btnShareAux);
